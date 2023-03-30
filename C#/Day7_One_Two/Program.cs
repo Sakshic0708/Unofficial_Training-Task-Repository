3c414027aa5cd03b8e45e04a8e7a9e631feb61f4
sakshi chauhan <sakshic0708@gmail.com>
2023-03-30 18:56:36 +0530

    Day 7 C sharp

---
+++ C#/Day7_One_Two/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_One_Two
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1.TaskOne();
+            Task2.TaskTwo();
+        }
+    }
+    public class Task1
+    {
+        public static void TaskOne()
+        {
+            Console.WriteLine("  Write a C# Sharp program to create a new list from a given list of integers where each integer value is added to 2 and the result value is multiplied by 5.");
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine("How Many List Element You Want in Array?");
+            int UserInput = Convert.ToInt32(Console.ReadLine());
+            List<int> originalList = new List<int>();
+            for (int i = 0; i < UserInput; i++)
+            {
+                Console.WriteLine("Enter List {0}'s:", i + 1);
+                originalList.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.WriteLine("List Elements Are:-");
+            foreach (int i in originalList)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Requirments of Task");
+            List<int> requiredList = test(new List<int>(originalList));
+            foreach (int i in requiredList)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static List<int> test(List<int> nums)
+        {
+            IEnumerable<int> e = nums.Select(x => 5 * (x + 2));
+            return e.ToList();
+        }
+    }
+    public class Task2
+    {
+        public static void TaskTwo()
+        {
+            var Number = new int[] { 1, 2, 4, 6, 7, 8, 9, 10, 12, 17, 19, 20, 21, 24, 25, 30 };
+
+            string[] groups = GroupPageNumbers(Number);
+
+            Console.WriteLine("Original: {0}", BuildList(Number.Select(p => p.ToString())));
+            Console.WriteLine("Grouped:  {0}", BuildList(groups));
+        }
+        static string[] GroupPageNumbers(int[] Number)
+        {
+            var groups = new List<string>();
+
+            for (int i = 0; i < Number.Length; i++)
+            {
+                int start = Number[i];
+                while (i < Number.Length - 1 && Number[i + 1] == Number[i] + 1)
+                {
+                    i++;
+                }
+                groups.Add(FormatGroup(start, Number[i]));
+            }
+
+            return groups.ToArray();
+        }
+
+        static string FormatGroup(int start, int end)
+        {
+            return start == end ? start.ToString() : string.Format("{0}-{1}", start, end);
+        }
+
+        static string BuildList(IEnumerable<string> items)
+        {
+            return string.Join(", ", items.ToArray());
+        }
+    }
+}
---
+++ C#/Day7_Task3/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Day7_Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            admission.Admission();
+        }
+    }
+    public class admission
+    {
+        private static object id;
+
+        public static void Admission()
+        {
+
+            Hashtable newAdmission = new Hashtable();
+            bool flag;
+            do
+            {
+                Console.WriteLine("Enter Student Details:-");
+
+                ArrayList studentList = new ArrayList();
+                {
+                    Console.WriteLine("Enter Name");
+                    var name = studentList.Add(Console.ReadLine());
+
+                    Console.WriteLine("Enter ID");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    newAdmission.Add(id, studentList);
+
+                    Console.WriteLine("Enter Phone Number");
+                    studentList.Add(Convert.ToInt64(Console.ReadLine()));
+
+                    Console.WriteLine("Enter Age");
+                    int age = Convert.ToInt32(Console.ReadLine());
+                    studentList.Add(age);
+
+                    Console.WriteLine("To Continue Press Y");
+                    Console.WriteLine("To Exit Press N");
+                    if (Console.ReadLine().ToUpper() == "Y")
+                    {
+                        flag = true;
+                    }
+                    else if (Console.ReadLine().ToUpper() == "N")
+                    {
+                        flag = false;
+                    }
+                    else
+                    {
+                        flag = false;
+                    }
+                }
+            }
+            while (flag);
+
+            foreach (DictionaryEntry show in newAdmission)
+            {
+                ArrayList studentList = (ArrayList)show.Value;
+
+                Console.WriteLine("Name is:" + studentList[0]);
+                Console.WriteLine("Phone Number is:" + studentList[1]);
+                Console.WriteLine("Age is:" + studentList[2]);
+                Console.WriteLine("\n");
+            }
+
+            Console.WriteLine("Total Elements: {0}", newAdmission.Count);
+
+            Console.WriteLine("Enter a Id Which You have Removing:-");
+            int RemoveId = Convert.ToInt32(Console.ReadLine());
+
+            newAdmission.Remove(RemoveId);
+  
+            Console.WriteLine("After Removing an Elements: {0}", newAdmission.Count);
+            foreach (DictionaryEntry show in newAdmission)
+            {
+                ArrayList studentList = (ArrayList)show.Value;
+
+                Console.WriteLine("Name is:" + studentList[0]);
+                Console.WriteLine("Phone Number is:" + studentList[1]);
+                Console.WriteLine("Age is:" + studentList[2]);
+                Console.WriteLine("\n");
+           
+            }
+            foreach (DictionaryEntry show in newAdmission)
+            {
+                ArrayList studentList = (ArrayList)show.Value;
+
+                Console.WriteLine("Best Award Goes To:-");
+                string awardname = Console.ReadLine();
+                Console.WriteLine("Best Award Goes to:-" +  awardname);
+
+            }
+           
+        }
+
+    }
+
+}
