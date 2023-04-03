2ac31dfa89f582610cb6112d54bacf2744bd8dc2
sakshi chauhan <sakshic0708@gmail.com>
2023-04-03 18:45:07 +0530

    Day 8 Task:- Validations

--- C#/Day8_EmployeeDirectory/Program.cs
+++ C#/Day8_EmployeeDirectory/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -58,133 +59,144 @@ namespace Day8_EmployeeDirectory
         }
         public void NewEmployee()
         {
-            bool flag;
-
-            do
+            List<Employee> employees = new List<Employee>();
+            bool addMore = true;
+            while (addMore)
             {
-                Console.WriteLine("Add Employee in JSON File");
+                bool flag;
 
                 do
                 {
-                    Console.WriteLine("Enter First Name");
-                    FirstName = Console.ReadLine();
-                }
-                while (!NameFormate(FirstName) || string.IsNullOrEmpty(FirstName));
+                    Console.WriteLine("Add Employee in JSON File");
 
-                do
-                {
-                    Console.WriteLine("Enter Last Name");
-                    LastName = Console.ReadLine();
-                }
-                while (!NameFormate(LastName) || string.IsNullOrEmpty(LastName));
-
-                do
-                {
-                    Console.Write("Enter Gender:-");
-                    Gender = Console.ReadLine();
-                    if (!string.IsNullOrEmpty(Gender))
+                    do
                     {
-                        if (Gender.ToLower() == "male")
-                        {
-                            Console.WriteLine("Male");
-                        }
-                        else if (Gender.ToLower() == "female")
-                        {
-                            Console.WriteLine("Female");
-                        }
-                        else if (Gender != "male" && Gender != "female")
-                        {
-                            Console.WriteLine("Only Male Or Female Are Allowed");
-                        }
+                        Console.WriteLine("Enter First Name");
+                        FirstName = Console.ReadLine();
+                    }
+                    while (!NameFormate(FirstName) || string.IsNullOrEmpty(FirstName));
 
+                    do
+                    {
+                        Console.WriteLine("Enter Last Name");
+                        LastName = Console.ReadLine();
+                    }
+                    while (!NameFormate(LastName) || string.IsNullOrEmpty(LastName));
+
+                    do
+                    {
+                        Console.Write("Enter Gender:-");
+                        Gender = Console.ReadLine();
+                        if (!string.IsNullOrEmpty(Gender))
+                        {
+                            if (Gender.ToLower() == "male")
+                            {
+                                Console.WriteLine("Male");
+                            }
+                            else if (Gender.ToLower() == "female")
+                            {
+                                Console.WriteLine("Female");
+                            }
+                            else if (Gender != "male" && Gender != "female")
+                            {
+                                Console.WriteLine("Only Male Or Female Are Allowed");
+                            }
+
+                        }
+                    }
+                    while (string.IsNullOrEmpty(Gender) || (Gender != "male" && Gender != "female"));
+
+                    do
+                    {
+                        Console.WriteLine("Enter Email Address");
+                        Email = Console.ReadLine();
+                    }
+                    while (!IsValidEmailAddress(Email) || string.IsNullOrEmpty(Email));
+                    string phone;
+                    do
+                    {
+                        Console.WriteLine("Enter Phone Number");
+                        phone = Console.ReadLine();
+                        PhoneNo = Convert.ToInt64(phone);
+                    }
+                    while (!IsValidPhoneNumber(phone));
+
+
+                    do
+                    {
+                        Console.WriteLine("Designation:-");
+                        Console.WriteLine("What Designation what You Choose:- Type 1-6 Any Number");
+
+                        Designation = Console.ReadLine();
+                        try
+                        {
+                            designation finalDesignation = (designation)Convert.ToInt32(Designation);
+                            Console.WriteLine(finalDesignation);
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Enter Valid Number");
+                            continue;
+                        }
+                    }
+                    while (string.IsNullOrEmpty(Designation));
+
+
+                    Console.WriteLine("Salary");
+                    Salary = Convert.ToInt32(Console.ReadLine());
+                    if (Salary >= 10000 && Salary <= 50000)
+                    {
+                        Console.WriteLine(Salary);
+                    }
+                    else
+                    {
+                        Console.WriteLine("InCorrect Amount");
+                    }
+
+
+                    //var EmployeeJSON = new Employee
+                    Employee emp = new Employee
+                    {
+                        FirstName = FirstName,
+                        LastName = LastName,
+                        Gender = Gender,
+                        Email = Email,
+                        PhoneNo = PhoneNo,
+                        Designation = Designation,
+                        Salary = Salary
+                    };
+                    employees.Add(emp);
+                   
+
+
+                    string json = JsonConvert.SerializeObject(employees, Formatting.Indented);
+                    File.WriteAllText("Employees.json", json);
+
+                    string jsonFromFile = File.ReadAllText("Employees.json");
+                    List<Employee> employeesFromFile = JsonConvert.DeserializeObject<List<Employee>>(jsonFromFile);
+                    Console.WriteLine("Here My all Data Of Employees Json File:");
+                    Console.WriteLine("----------------------------------------------");
+                    foreach (Employee emps in employeesFromFile)
+                    {
+                        Console.WriteLine(emps.ToString());
+                    }
+                   
+                    Console.WriteLine("Add more employees? (Y/N): ");
+                    if (Console.ReadLine().ToUpper() == "Y")
+                    {
+                        flag = true;
+                    }
+                    else if (Console.ReadLine().ToUpper() == "N")
+                    {
+                        flag = false;
+                    }
+                    else
+                    {
+                        flag = false;
                     }
                 }
-                while (string.IsNullOrEmpty(Gender) || (Gender != "male" && Gender != "female"));
-
-                do
-                {
-                    Console.WriteLine("Enter Email Address");
-                    Email = Console.ReadLine();
-                }
-                while (!IsValidEmailAddress(Email) || string.IsNullOrEmpty(Email));
-                string phone;
-                do
-                {
-                    Console.WriteLine("Enter Phone Number");
-                    phone = Console.ReadLine();
-                    PhoneNo = Convert.ToInt64(phone);
-                }
-                while (!IsValidPhoneNumber(phone));
-
-
-                do
-                {
-                    Console.WriteLine("Designation:-");
-                    Console.WriteLine("What Designation what You Choose:- Type 1-6 Any Number");
-
-                    Designation = Console.ReadLine();
-                    try
-                    {
-                        designation finalDesignation = (designation)Convert.ToInt32(Designation);
-                        Console.WriteLine(finalDesignation);
-                    }
-                    catch (FormatException)
-                    {
-                        Console.WriteLine("Enter Valid Number");
-                        continue;
-                    }
-                }
-                while (string.IsNullOrEmpty(Designation));
-
-
-                Console.WriteLine("Salary");
-                Salary = Convert.ToInt32(Console.ReadLine());
-                if (Salary >= 10000 && Salary <= 50000)
-                {
-                    Console.WriteLine(Salary);
-                }
-                else
-                {
-                    Console.WriteLine("InCorrect Amount");
-                }
-
-
-                var EmployeeJSON = new Employee
-                {
-                    FirstName = FirstName,
-                    LastName = LastName,
-                    Gender = Gender,
-                    Email = Email,
-                    PhoneNo = PhoneNo,
-                    Designation = Designation,
-                    Salary = Salary
-                };
-
-                var json = JsonConvert.SerializeObject(EmployeeJSON);
-                File.WriteAllText("Employee.json", json);
-
-                var JsonFromFile = File.ReadAllText("Employee.json");
-                var PersonFromFile = JsonConvert.DeserializeObject<Employee>(JsonFromFile);
-                Console.WriteLine(PersonFromFile.ToString());
-
-
-                Console.WriteLine("To Continue Press Y");
-                Console.WriteLine("To Exit Press N");
-                if (Console.ReadLine().ToUpper() == "Y")
-                {
-                    flag = true;
-                }
-                else if (Console.ReadLine().ToUpper() == "N")
-                {
-                    flag = false;
-                }
-                else
-                {
-                    flag = false;
-                }
-
+                while (flag);
             }
-            while (flag);
         }
 
 
